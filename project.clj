819f61bf78c4b8a385ceae5f07802a93a2369889
Jeremy Heiler <jeremyheiler@gmail.com>
2013-06-24 18:07:17 -0400

    Add project.clj and .gitignore files

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lime.smtp.server "0.1.0-SNAPSHOT"
+  :description "A Clojure SMTP Server"
+  :url "https://github.com/limemail/lime.smtp.server"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
